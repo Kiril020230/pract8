15c5ebc1c2b474c1b7a24750e8c6232957870fd2
naswaichik <nahov.dm@gmail.com>
2024-10-19 11:58:22 +0400

    Добавьте файлы проекта.

---
+++ UserProfileForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace pract8
+{
+    public partial class UserProfileForm : Form
+    {
+        public UserProfileForm(string username, string email)
+        {
+            InitializeComponent();
+            labelUsername.Text = $"Добро пожаловать, {username}!";
+            labelEmail.Text = $"Ваша почта: {email}";
+        } 
+
+        private void buttonLogout_Click(object sender, EventArgs e)
+        {
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+
+            // Закрытие текущей формы (UserProfileForm)
+            this.Close();
+        }
+    }
+}
